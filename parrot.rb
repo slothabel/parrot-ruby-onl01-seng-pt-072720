cc2d147ce62444db47d0f01a4935654866ff41a0
Yoshi Mejia <yoshimejia00@gmail.com>
2020-07-29 01:31:20 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,17 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+def parrot
+  puts "Squawk!"
+end
+
+def parrot(phrase="Squawk!")
+  return "#{phrase}"
+end
+
+def parrot(phrase="Pretty bird!")
+  puts "#{phrase}"
+end
+
+def parrot(phrase="Pretty bird!")
+  return "#{phrase}"
+end
